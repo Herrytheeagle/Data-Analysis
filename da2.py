115f4dff5b0c9ca04cfb82c33e1f1fda05800d9c
Heritage Samuel F <herrytheeagle@gmail.com>
2018-11-09 09:13:42 +0100

    imprvd

--- da2.py
+++ da2.py
@@ -3,4 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
 
-web_stats = {'Day': [1, 2, 3, 4, 5, 6]}
+web_stats = {'Day': [1, 2, 3, 4, 5, 6],
+             'Visitors': [43, 53, 34, 45, 64, 34],
+             'Bounce_Rate': [65, 73, 63, 65, 55, 77]}
+
+df = pd.DataFrame(web_stats)
+print(df)
+
